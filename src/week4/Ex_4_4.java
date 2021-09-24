e52b77d3e1279888b90f9bf046021ea5b4e0d794
Chisan Ahn <chisan2001@naver.com>
2021-09-24 19:32:12 +0900

    0924 수업중 실습

---
+++ src/week4/Ex_4_4.java
@@ -0,0 +1,21 @@
+package week4;
+
+class Book {
+    String title;
+    String author;
+    public Book(String t) {
+        title = t; author = "작자미상";
+    }
+    public Book(String t, String a) {
+        title = t; author = a;
+    }
+}
+
+public class Ex_4_4 {
+    public static void main(String [] args) {
+        Book littlePrince = new Book("어린왕자", "생텍쥐페리");
+        Book loveStory = new Book("춘향전");
+        System.out.println(littlePrince.title + " " + littlePrince.author);
+        System.out.println(loveStory.title + " " + loveStory.author);
+    }
+}
